fc1619a7cd8b681f1bb8c802d114c69a6179ddac
Anik-the-dev <niksctg00@gmail.com>
2022-06-09 11:09:11 +0600

    cardand grid layout done

--- src/Pages/MyHome/Portfolio.js
+++ src/Pages/MyHome/Portfolio.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
 
 const Portfolio = () => {
     return (
-        <div>
+        <Container>
+            <h2 className='custom-dark-color custom-font-weight-600 fs-1 text-center'>Have a look at my <span className='custom-primary-color'>React </span>Projects</h2>
+            <div className='custom-container mt-5 '>
+                <div className='custom-grid my-5'>
+                    <div className='custom-card'>
+                        
+                    </div>
+                    <div>2</div>
+                    <div>3</div>
+                </div>
+
+            </div>
             
-        </div>
+        </Container>
     );
 };
 
